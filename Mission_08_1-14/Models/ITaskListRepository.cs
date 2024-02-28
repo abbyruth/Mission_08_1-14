57857fc0a43e1b11fafffb26ff2210ff050641f9
Seth Brock <sab716@byu.edu>
2024-02-28 10:58:23 -0700

    Minor update to ITaskListRepository

--- Mission_08_1-14/Models/ITaskListRepository.cs
+++ Mission_08_1-14/Models/ITaskListRepository.cs
@@ -1,11 +1,15 @@
 ﻿namespace Mission_08_1_14.Models {
     public interface ITaskListRepository {
         // Task Methods
+        IQueryable<Task> Tasks { get; }
         public void AddTask(Task task);
+        public void UpdateTask(Task task);
         public void RemoveTask(Task task);
 
         // Category Methods
+        IQueryable<Category> Categories { get; }
         public void AddCategory(Category category);
+        public void UpdateCategory(Category category);
         public void RemoveCategory(Category category);
 
     }
